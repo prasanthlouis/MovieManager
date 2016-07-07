2eb8facfd31d6716f7468f46e2a768b352281a7b
Prasanth Louis <prasanthlouis21@gmail.com>
2016-07-07 15:56:36 -0400

    Fixed some bugs

---
+++ MovieManager/Controllers/MovieManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieManager.Controllers
+{
+    public class MovieManagerController : Controller
+    {
+        // GET: MovieManager
+        public ActionResult Movie()
+        {
+            return View();
+        }
+    }
+}
